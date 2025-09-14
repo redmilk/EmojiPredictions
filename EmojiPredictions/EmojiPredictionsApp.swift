95daf265173b68794d6d111f8224c879f24d507b
Danyl Timofeyev <timofeev.danil@gmail.com>
2025-09-14 19:34:47 +0100

    Initial Commit

---
+++ EmojiPredictions/EmojiPredictionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  EmojiPredictionsApp.swift
+//  EmojiPredictions
+//
+//  Created by Danyl Timofeyev on 14.09.2025.
+//
+
+import SwiftUI
+
+@main
+struct EmojiPredictionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
